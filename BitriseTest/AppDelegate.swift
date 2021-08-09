134f42776034839af1592aebe45f9342c2f7e06a
Alejandro.Castillo <alejandro.castillo@fluxit.com.ar>
2021-08-09 19:55:05 -0300

    initial commit

---
+++ BitriseTest/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  BitriseTest
+//
+//  Created by Alejandro Castillo on 09/08/2021.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        let window = UIWindow(frame: UIScreen.main.bounds)
+        self.window = window
+        let initialVC = UINavigationController(rootViewController: MainViewController())
+        window.rootViewController = initialVC
+        window.makeKeyAndVisible()
+        
+        return true
+    }
+}
+
